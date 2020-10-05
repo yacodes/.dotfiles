55369660a8ea97733ace66fccc48bb584640a8bf
Aleksandr Yakunichev <hi@ya.codes>
2020-10-05 19:30:40 +0300

    Update web-mode auto-mode-alist

--- init.el
+++ init.el
@@ -195,6 +195,9 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
   (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
   (add-hook 'web-mode-hook  'web-mode-init-hook)
   (setq-default js-indent-level 2
