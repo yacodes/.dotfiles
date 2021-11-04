4a0a49e167e3375a59d5286ea8b4d317e7e4b404
Aleksandr Yakunichev <hi@ya.codes>
2021-11-04 23:04:57 +0300

    Update install.sh with force linking

--- install.sh
+++ install.sh
@@ -3,32 +3,32 @@
 echo "Installing .dotfiles..."
 
 echo "Linking .gitconfig..."
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo "Linking .zshrc..."
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -fs ~/.dotfiles/.zshrc ~/.zshrc
 
 echo "Linking i3..."
 mkdir -p ~/.config/i3/
-ln -s ~/.dotfiles/i3 ~/.config/i3/config
+ln -fs ~/.dotfiles/i3 ~/.config/i3/config
 
 echo "Linking picom..."
 mkdir -p ~/.config/picom/
-ln -s ~/.dotfiles/picom.conf ~/.config/picom/picom.conf
+ln -fs ~/.dotfiles/picom.conf ~/.config/picom/picom.conf
 
 echo "Linking rofi..."
 mkdir -p ~/.config/rofi/
-ln -s ~/.dotfiles/rofi.rasi ~/.config/rofi/config.rasi
+ln -fs ~/.dotfiles/rofi.rasi ~/.config/rofi/config.rasi
 
 echo "Linking polybar..."
 mkdir -p ~/.config/polybar/
-ln -s ~/.dotfiles/polybar.ini ~/.config/polybar/config.ini
+ln -fs ~/.dotfiles/polybar.ini ~/.config/polybar/config.ini
 
 echo "Linking polybar launch..."
-ln -s ~/.dotfiles/launch-polybar.sh ~/.config/polybar/launch.sh
+ln -fs ~/.dotfiles/launch-polybar.sh ~/.config/polybar/launch.sh
 
 echo "Linking emacs..."
 mkdir -p ~/.emacs.d/
-ln -s ~/.dotfiles/init.el ~/.emacs.d/init.el
+ln -fs ~/.dotfiles/init.el ~/.emacs.d/init.el
 
 echo "Done."
