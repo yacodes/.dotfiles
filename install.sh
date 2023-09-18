b48a6c1f3717780056b71b54d0a0615cecfcc97d
Aleksandr Yakunichev <hi@ya.codes>
2023-09-18 22:25:14 +0200

    Remove unused configs

--- install.sh
+++ install.sh
@@ -12,20 +12,9 @@ echo "Linking i3..."
 mkdir -p ~/.config/i3/
 ln -fs ~/.dotfiles/i3 ~/.config/i3/config
 
-echo "Linking picom..."
-mkdir -p ~/.config/picom/
-ln -fs ~/.dotfiles/picom.conf ~/.config/picom/picom.conf
-
 echo "Linking rofi..."
 mkdir -p ~/.config/rofi/
 ln -fs ~/.dotfiles/rofi.rasi ~/.config/rofi/config.rasi
-
-echo "Linking polybar..."
-mkdir -p ~/.config/polybar/
-ln -fs ~/.dotfiles/polybar.ini ~/.config/polybar/config.ini
-
-echo "Linking polybar launch..."
-ln -fs ~/.dotfiles/launch-polybar.sh ~/.config/polybar/launch.sh
 
 echo "Linking emacs..."
 mkdir -p ~/.emacs.d/
